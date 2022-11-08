c5eea9d82f28009b9d0070efedce295894e53a3a
Maliek2 <104944194+Maliek2@users.noreply.github.com>
2022-11-08 15:04:24 +0200

    Update normal.tsx

--- pages/normal.tsx
+++ pages/normal.tsx
@@ -1,3 +1,45 @@
 export default function NormalPage() {
-  return <p>I'm just a normal old page, no AMP for me</p>
+  return 
+
+<p>I'm just a normal old page, no AMP for me</p>
+<div>
+      <amp-script layout="fixed-height" height="36" script="user-agent-script" class="sample">
+        <div>
+          Your operating system is:
+          <span id="operating-system" class="answer-text"></span>
+        </div>
+      </amp-script>
+
+      <script id="user-agent-script" type="text/plain" target="amp-script">
+        // Adapted with gratitude from https://stackoverflow.com/a/38241481
+
+        function getOS() {
+          const userAgent = navigator.userAgent,
+                platform = navigator.platform,
+                macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
+                windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
+                iosPlatforms = ['iPhone', 'iPad', 'iPod'];
+
+          if (macosPlatforms.includes(platform)) {
+            return 'Mac OS';
+          } else if (iosPlatforms.includes(platform)) {
+            return 'iOS';
+          } else if (windowsPlatforms.includes(platform)) {
+            return 'Windows';
+          } else if (/Android/.test(userAgent)) {
+            return 'Android';
+          } else if (/Linux/.test(platform)) {
+            return 'Linux';
+          }
+
+          return 'Unknown';
+        }
+
+        const span = document.getElementById('operating-system');
+        span.textContent = getOS();
+      </script>
+    </div>
+
+
+
 }
